2c69203e158c64062466badba71278496649f967
jomigalves <jma.alves@zoho.com>
2020-10-30 20:37:30 +0000

    Submission #1

--- assignment.R
+++ assignment.R
@@ -4,24 +4,30 @@ library(dplyr)
 
 ## First step ------
 ## import the sample data from the following path: "data/sample_data.csv"
-
+sample_data <- read_csv("data/sample_data.csv")
 
 
 ## Second step ------
 ## select the observations from the variable `Region` that does not contain `Central`
-
+filtered_data <- filter(sample_data, Region != "Central")
 
 
 ## Third step -------
 ## remove the variable `Total`
-
+selected_data <- select(filtered_data, Region:`Unit Cost`)
 
 
 ## Fourth step -------
 ## rearrange the data by `Unit Cost`, ascending order
+arranged_data <- arrange(selected_data, `Unit Cost`)
+
+
+##----- or in 1 step, using pipe----
+#arranged_data2 <- sample_data %>% filter(Region != "Central") %>%
+#  select(Region:`Unit Cost`) %>%
+#  arrange(`Unit Cost`)
 
 
 
 ## Save your final dataset:
-##
 save(dataset, file = "output.rda", version = 2)
